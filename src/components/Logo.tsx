c71980c7798e7b79bb52399f215df77640c40ed1
Richmond <richmondoghene@gmail.com>
2026-04-13 20:57:09 +0100

    added logo from assets

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -17,7 +17,7 @@ export default function Logo({ className, iconOnly = false, variant = "default" 
       <img
   src={logo}
   alt="Logo"
-  className="w-10 h-10 object-contain"
+  className="w-20 h-20 object-contain"
 />
 
       {!iconOnly && (
